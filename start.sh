31ba7b3cc2b3ac27bf59895ed74ad01741e522b7
Tom Nussbaumer <thomas.nussbaumer@gmx.net>
2015-09-03 13:28:09 +0200

    change due to entrypoint and handling of beta version

--- start.sh
+++ start.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
-## maps host port 10000 to container port 4000
-## maps project directory to /project within container
-
-docker run -ti --rm -v "$(pwd)":/project -p 127.0.0.1:30000:4000 $(cat REPO_AND_VERSION) \
-       /sbin/my_init -- /sbin/remapuser app $(id -u) $(id -g) bash 
+if [ $# -eq 0 ]; then
+  # (*) map container port 4000 to port 4000 on all host interfaces
+  # (*) mounts current working directory to "/project" within container
+  # (*) remaps container user 'app' to uid/gid of user starting the script
+  # (*) starts jekyll server in current working directory
+  docker run -ti --rm -v "$(pwd)":/project -p 4000:4000 -w /project \
+         sys42/docker-jekyll3:latest \
+         /sbin/remapuser app $(id -u) $(id -g) \
+         /bin/bash -lic "js"
+else
+  # If arguments are given to this script they are interpreted as
+  # commands to run within the container.
+  # To start an interactive shell just use: 
+  #
+  #  ./start.sh bash
+  #
+  docker run -ti --rm -v "$(pwd)":/project -p 4000:4000 -w /project \
+         sys42/docker-jekyll3:latest \
+         /sbin/remapuser app $(id -u) $(id -g) \
+         "$@"
+fi
